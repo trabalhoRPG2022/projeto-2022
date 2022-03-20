fc1080bf68a22f382949afd79b1446da5704d95c
MachadomxX <83429626+MachadomxX@users.noreply.github.com>
2022-03-20 20:41:29 -0300

    Co-authored-by: NikolasTrapp <NikolasTrapp@users.noreply.github.com> Co-authored-by: VIRGULA <virgula-web@users.noreply.github.com>

--- teste/sprites.py
+++ teste/sprites.py
@@ -1,5 +1,7 @@
 import pygame as pg
 from configs import *
+from arvore import Arvore
+
 
 class Player(pg.sprite.Sprite):
     
@@ -7,7 +9,12 @@ class Player(pg.sprite.Sprite):
         self.game = game
         self._layer = player_layer
         self.groups = self.game.all_sprites
+        self.mostrar_chao = pg.display.get_surface()
+        self.obstaculos_visiveis = pg.sprite.Group()
+        self.obstaculos = pg.sprite.Group()
         pg.sprite.Sprite.__init__(self, self.groups)
+        self.criar_mapa()
+
 
         self.x = x * tile_size
         self.y = y * tile_size
@@ -23,9 +30,10 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
+        
 
-        image = pg.image.load('images\personagem.png')
-
+        image = pg.image.load('img/personagem.png')
+        
         self.image = pg.Surface([self.width, self.height])
         self.image.set_colorkey(preto)
         self.image.blit(image, (0,0))
@@ -53,3 +61,15 @@ class Player(pg.sprite.Sprite):
 
         if keys[pg.K_s]: 
             self.y_change += speed
+        
+    def criar_mapa(self):
+        for obs_index,obs in enumerate(MAPA_MUNDO):
+            for coluna_index, coluna in enumerate(obs):
+                x =  coluna_index * tile_size
+                y =  obs_index * tile_size
+                if coluna == 'x':
+                    Arvore((x,y), [self.obstaculos_visiveis])
+                    
+    
+    def run(self):
+        self.obstaculos_visiveis.draw(self.mostrar_chao)
